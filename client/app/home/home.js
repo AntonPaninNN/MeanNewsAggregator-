38c57f1396b24571e290d54ebde95a3988147e7e
veles83 <veles83@gmail.com>
2016-02-27 13:44:49 +0300

    Initial Commit

---
+++ client/app/home/home.js
@@ -0,0 +1,8 @@
+angular.module('mean.home', [])
+  .controller('HomeController', function($scope, $http){
+
+   var init = function() {console.log('home controller up and going');};
+   
+   init();
+
+  });
