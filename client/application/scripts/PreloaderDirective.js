c6740371938710c4ae5cfaf5ca0fa9308a5466b7
hartex <kluyev.nikolay@gmail.com>
2016-03-26 18:25:44 +0300

    added preloader directive

---
+++ client/application/scripts/PreloaderDirective.js
@@ -0,0 +1,24 @@
+angular
+    .module("news-aggregator-app")
+    .directive('mnaPreloader', ['$log', function($log) {
+        return {
+            restrict: 'E',
+            template: '<img src="img/715.gif"/>',
+            //transclude: true,
+            replace: true,
+            scope: {
+                isDataLoading: "@isDataLoading"
+            },
+            link: function link(scope, element, attrs, controller, transcludeFn) {
+                scope.$watch("isDataLoading", function(newValue){
+                    if (newValue === "true"){
+                        $(element[0]).show();
+                    } else {
+                        $(element[0]).hide();
+                    }
+                    //$log.log("element inside directive: " + element[0]);
+                    //$log.log("isDataLoading inside directive: " + newValue, oldValue);
+                });
+            }
+        };
+    }]);
